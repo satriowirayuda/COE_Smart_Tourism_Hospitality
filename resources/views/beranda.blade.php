d1e0485e1e9e4ef0462de02ad683c0285ae7af1e
Mariana <marmar.smsr@gmail.com>
2024-09-06 20:52:23 +0700

    section news & timeline di homepage

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -9,17 +9,16 @@
 </head>
 <body>
     <section class="relative bg-cover bg-center h-screen" style="background-image: url('{{ asset('assets/bhomepage.png') }}')">
-    <div class="absolute inset-0 bg-black bg-opacity-70"></div>
+        <div class="absolute inset-0 bg-black bg-opacity-70"></div>
         <div class="relative text-center text-white flex flex-col justify-center items-center h-full">
-        <div class="font-bold">
-            <p class="text-6xl">Center of Excellence</p>
-            <p class="text-7xl">Smart Tourism & Hospitality</p>
+            <div class="font-bold">
+                <p class="text-6xl">Center of Excellence</p>
+                <p class="text-7xl">Smart Tourism & Hospitality</p>
+            </div>
+            <div class="px-[300px] pt-5">
+                <p class="text-xl text-[#BBBBBB]">Tingkatkan pengalaman Anda dengan Smart Tourism dan Hospitality! Hemat waktu, jelajahi lebih banyak, dan rasakan kenyamanan teknologi.</p>
+            </div>
         </div>
-        <div class="px-[300px] pt-5">
-            <p class="text-xl text-[#BBBBBB]">Tingkatkan pengalaman Anda dengan Smart Tourism dan Hospitality! Hemat waktu, jelajahi lebih banyak, dan rasakan kenyamanan teknologi.</p>
-        </div>
-        </div>
-        
     </section>
 
     <section class="w-full bg-background p-10 px-36 py-24">
@@ -70,11 +69,10 @@
                 <a href="#PengabdianMasyarakat" class="absolute inset-0"></a>
             </div>
         </div>
-        
     </section>
 
     <section class="px-36">
-        <div class="py-40">
+        <div class="pt-40">
         <p class="text-3xl font-bold pb-10">Project Terkini</p>
         <div class="bg-gray-200 py-10 px-10 rounded-[22px] w-full flex flex-col sm:flex-row gap-x-14 shadow-xl">
   
@@ -85,7 +83,8 @@
                     title="Youtube Video Player"
                     frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowfullscreen></iframe>
+                    allowfullscreen>
+                </iframe>
             </div>
 
             <div class="flex-1">
@@ -100,12 +99,162 @@
                     </button>
                 </a>
             </div>
-
         </div>
-        
         </div>
-        
     </section>
 
+    <section class="px-36 py-40">
+        <div class="grid grid-cols-2 gap-x-24">
+            <div class="grid-cols-1">
+                <p class="text-4xl font-bold drop-shadow-5xl text-center pb-10">News</p>
+
+                <!-- <div class="">
+                    <img class="w-[500.95px] rounded-[15px]"  src="{{ asset('assets/artikel.png') }}" alt="Berita">
+                </div> -->
+
+
+                <div class="relative">
+                    <div class="rounded-[15px] overflow-hidden">
+                        
+                        <img src="{{ asset('assets/artikel.png') }}" alt="Berita" class=" rounded-[15px] w-[500.95px] h-auto">
+                        <div class="absolute inset-0 bg-black bg-opacity-30 rounded-[15px]"></div>
+
+                        <div class="text-white absolute top-0 right-0 p-5 text-base">
+                            <a href="#" target="_blank">
+                                Selengkapnya
+                                <i class="fas fa-arrow-right ml-1"></i>
+                            </a>
+                        </div>
+
+                        <div class="absolute bottom-0 left-0 w-full text-white p-5">
+                            <p class="text-4xl font-bold">Judul Berita</p>
+                            <p class="text-base">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div  class="grid-cols-1">
+                <p class="text-4xl font-bold text-center drop-shadow-5xl pb-10">Timeline</p>
+                <div class="mr-0">
+                <div>
+                    <p class="text-[13px] italic font-bold mb-2">Event LKM</p>
+                    <table class="w-full table-auto mb-3">
+
+                        <thead>
+                        <tr class="text-[13px] bg-[#626262] text-white">
+                            <th class="text-left py-2 pl-6">ID</th>
+                            <th class="text-left py-2 px-2 w-48 ">Event Name</th>
+                            <th class="text-left py-2 px-2">Start Date</th>
+                            <th class="text-left py-2 px-2">End Date</th>
+                        </tr>
+                        </thead>
+                        
+                        <tr class=" text-[9px] odd:bg-[#EEEEEE] even:bg-[#D9D9D9]">
+                            <td class="py-1 pl-6"></td>
+                            <td class="py-1 px-2">Sosialisasi Pengumpulan Berkas Magang Genap 23-24</td>
+                            <td class="py-1 px-2">22 May 2024</td>
+                            <td class="py-1 px-2">22 May 2024</td>
+                        </tr>
+                        <tr class="text-[9px] odd:bg-[#EEEEEE] even:bg-[#D9D9D9]">
+                            <td class="py-1 pl-6"></td>
+                            <td class="py-1 px-2">Webinar Career Hack & Sosialisasi Magang Seven Inc</td>
+                            <td class="py-1 px-2">22 May 2024</td>
+                            <td class="py-1 px-2">22 May 2024</td>
+                        </tr>
+                        <tr class="text-[9px] odd:bg-[#EEEEEE] even:bg-[#D9D9D9]">
+                            <td class="py-1 pl-6"></td>
+                            <td class="py-1 px-2">Sosialisasi Magang 1 & 2 Semester TA 2024/2025</td>
+                            <td class="py-1 px-2">22 May 2024</td>
+                            <td class="py-1 px-2">22 May 2024</td>
+                        </tr>
+                        <tr class="text-[9px] odd:bg-[#EEEEEE] even:bg-[#D9D9D9]">
+                            <td class="py-1 pl-6"></td>
+                            <td class="py-1 px-2">Sosialisasi Pengumpulan Berkas Magang Ganjil 23-24</td>
+                            <td class="py-1 px-2">22 May 2024</td>
+                            <td class="py-1 px-2">22 May 2024</td>
+                        </tr>
+                        </div>
+                    </table>
+                </div>
+                <div>
+                    <p class="text-[13px] italic font-bold mb-2">Event External</p>
+                    <table class="w-full table-auto">
+
+                        <thead>
+                        <tr class="text-[13px] bg-[#626262] text-white">
+                            <th class="text-left py-2 pl-6">ID</th>
+                            <th class="text-left py-2 px-2 w-48 ">Event Name</th>
+                            <th class="text-left py-2 px-2">Start Date</th>
+                            <th class="text-left py-2 px-2">End Date</th>
+                        </tr>
+                        </thead>
+                        
+                        <tr class=" text-[9px] odd:bg-[#EEEEEE] even:bg-[#D9D9D9]">
+                            <td class="py-1 pl-6"></td>
+                            <td class="py-1 px-2">Sosialisasi Pengumpulan Berkas Magang Genap 23-24</td>
+                            <td class="py-1 px-2">22 May 2024</td>
+                            <td class="py-1 px-2">22 May 2024</td>
+                        </tr>
+                        <tr class="text-[9px] odd:bg-[#EEEEEE] even:bg-[#D9D9D9]">
+                            <td class="py-1 pl-6"></td>
+                            <td class="py-1 px-2">Webinar Career Hack & Sosialisasi Magang Seven Inc</td>
+                            <td class="py-1 px-2">22 May 2024</td>
+                            <td class="py-1 px-2">22 May 2024</td>
+                        </tr>
+                        <tr class="text-[9px] odd:bg-[#EEEEEE] even:bg-[#D9D9D9]">
+                            <td class="py-1 pl-6"></td>
+                            <td class="py-1 px-2">Sosialisasi Magang 1 & 2 Semester TA 2024/2025</td>
+                            <td class="py-1 px-2">22 May 2024</td>
+                            <td class="py-1 px-2">22 May 2024</td>
+                        </tr>
+                        <tr class="text-[9px] odd:bg-[#EEEEEE] even:bg-[#D9D9D9]">
+                            <td class="py-1 pl-6"></td>
+                            <td class="py-1 px-2">Sosialisasi Pengumpulan Berkas Magang Ganjil 23-24</td>
+                            <td class="py-1 px-2">22 May 2024</td>
+                            <td class="py-1 px-2">22 May 2024</td>
+                        </tr>
+                        </div>
+                    </table>
+                </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
+
+<footer class="w-full bg-[#2593CF] text-white pb-28">
+    <div class="grid grid-cols-3 px-36 pt-16 gap-16">
+        <div class="grid-cols-1">
+            <p class="text-xl font-semibold">Center of Excellence Smart Tourism & Hotpitality</p>
+            <p class="text-sm pt-7 pr-8">Jl. Telekomunikasi No.01, Terusan Buahbatu</p>
+        </div>
+        <div class="grid-cols-1">
+            <p class="text-xl font-semibold">Akses Cepat</p>
+            <div class="text-sm pt-[55px] space-y-3">
+                <p><a href="#">Home</a></p>
+                <p><a href="#">Tentang Kami</a></p>
+                <p><a href="#">Aktivitas</a></p>
+                <p><a href="#">Program</a></p>
+            </div>
+        </div>
+        <div class="grid-cols-1">
+            <p class="text-xl font-semibold">Kontak</p>
+            <div class="flex space-x-3 pt-[55px]">
+                <a href="" target="_blank">
+                    <img src="{{ asset('assets/Call-Button.svg') }}" alt="Telepon" class="w-[40px] h-[40px]">
+                </a>
+                <a href="" target="_blank">
+                    <img src="{{ asset('assets/Facebook-Button.svg') }}" alt="Facebook" class="w-[40px] h-[40px]">
+                </a>
+                <a href="https://www.instagram.com/coe_smarttourism/" target="_blank">
+                    <img src="{{ asset('assets/IG-Button.svg') }}" alt="Instagram" class="w-[40px] h-[40px]">
+                </a>
+                <a href="" target="_blank">
+                    <img src="{{ asset('assets/Linked-In-Button.svg') }}" alt="LinkedIn" class="w-[40px] h-[40px]">
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
 </html>
