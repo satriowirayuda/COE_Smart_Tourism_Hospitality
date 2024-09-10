f3788421d426146ca1fc1ab04b238dc0ed3392e6
Darel Ajni Fahrezi <darelfahrezi@gmail.com>
2024-09-10 18:52:25 +0700

    Membuat api untuk timeline

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NewsController;
+use App\Http\Controllers\EventController;
 use App\Http\Controllers\ProjectController;
 
 Route::get('/user', function (Request $request) {
@@ -17,4 +18,9 @@ Route::get('/news', [NewsController::class, 'index']);
 Route::post('/news', [NewsController::class, 'store']);
 Route::put('/news/{news}', [NewsController::class, 'update']);
 Route::delete('/news/{news}', [NewsController::class, 'delete']);
+
+Route::get('/events', [EventController::class, 'index']);
+Route::post('/events', [EventController::class, 'store']);
+Route::put('/events/{event}', [EventController::class, 'update']);
+Route::delete('/events/{event}', [EventController::class, 'delete']);
 
