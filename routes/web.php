083262ebb20b6bdb8cf5f154cb276eb74a0dcfcc
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2024-09-17 13:17:09 +0700

    Menambahkan assets

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,29 @@ Route::get('/sejarah', function () {
 Route::get('/anggota', function () {
     return view('member.anggota-page');
 });
+
+Route::get('/login', function () {
+    return view('admin.login');
+});
+
+Route::get('/dashboard', function () {
+    return view('admin.dashboard');
+});
+
+
+Route::get('/project', function () {
+    return view('admin.project.project-terkini');
+});
+
+Route::get('/aktivitas', function () {
+    return view('admin.aktivitas.aktivitas');
+});
+
+
+Route::get('/news', function () {
+    return view('admin.aktivitas.berita.berita');
+});
+
+Route::get('/upload-news', function () {
+    return view('admin.aktivitas.berita.upload-news');
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        default: ["Lota Grotesque Alt 2"],
-        "paragraf-sth" : "Lota Grotesque Alt 1"
-      },
-      colors: {
-        "background": "#EEEEEE",
-        "biru-sth": "#2593CF",
-        "putih-sth": "#FFFFFF",
-        "pink-sth": "#EC018C"
-      },
-      fontSize: {
-        "size-24": "text-xl"
-      }
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                default: ["Lota Grotesque Alt 2"],
+                "paragraf-sth": "Lota Grotesque Alt 1",
+            },
+            colors: {
+                background: "#EEEEEE",
+                "biru-sth": "#2593CF",
+                "putih-sth": "#FFFFFF",
+                "pink-sth": "#EC018C",
+                "biru-form": "#0095DC",
+            },
+            fontSize: {
+                "size-24": "text-xl",
+            },
+        },
+        
     },
-  },
-  plugins: [],
-}
-
+    plugins: [],
+};
