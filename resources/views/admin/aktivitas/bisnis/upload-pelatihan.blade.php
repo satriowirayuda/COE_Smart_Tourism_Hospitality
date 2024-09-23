7a696bb1e4474c14a5dd13c97fe895e39684e9c8
Mhd. Ananda Ridho Alfadillah <fadil.fadil072017@gmail.com>
2024-09-23 10:31:51 +0700

    Membuat halaman bisnis(admin)

---
+++ resources/views/admin/aktivitas/bisnis/upload-pelatihan.blade.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-blue-400 min-h-screen flex justify-center">
+    <div class="w-full bg-white rounded-2xl my-4 mx-4 shadow-lg flex flex-col">
+        <div class="flex items-center pl-10 py-5">
+            <img src="{{ asset('assets/logo.png') }}" alt="Logo" class="w-8 h-10 object-cover">
+            <h2 class="pl-6 font-semibold text-xl">Halaman Admin</h2>
+        </div>
+        <hr class="border-grey-300 border" />
+
+        <!-- Wrapper untuk sidebar dan konten utama -->
+        <div class="flex flex-1 overflow-hidden">
+            <!-- Panggil Sidebar -->
+            <x-sidebar />
+
+            <!-- Konten Utama di Sebelah Sidebar -->
+            <div class="px-10 py-10 w-full">
+                <h2 class="font-semibold text-2xl">Manajemen Tambahan Pelatihan</h2>
+                <p>Isi konten untuk tambah pelatihan</p>
+
+                <form action={{ route('upload_training') }} method="POST" enctype="multipart/form-data" class="pt-10">
+                    <div class="flex justify-between">
+                        <!-- Bagian untuk input teks -->
+                        <div class="flex-1 space-y-4">
+                            <!-- Judul Pelatihan-->
+                            <div class="mb-4">
+                                <label for="title" class="block text-sm font-medium text-gray-700">Judul
+                                    Pelatihan</label>
+                                <input type="text" id="title" name="title"
+                                    class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-amber-500 focus:border-amber-500 sm:text-sm"
+                                    required>
+                            </div>
+
+                            <!-- Deskripsi Pelatihan -->
+                            <div class="mb-4">
+                                <label for="description" class="block text-sm font-medium text-gray-700">Deskripsi
+                                    Pelatihan</label>
+                                <textarea id="description" name="description" rows="4"
+                                    class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-amber-500 focus:border-amber-500 sm:text-sm"
+                                    required></textarea>
+                            </div>
+
+                            <!-- Tanggal Pelatihan, lokasi dan jenis pelatihan -->
+                            <div class="flex gap-4 w-[85%]">
+                                <!-- Tanggal Pelatihan -->
+                                <div class="mb-4 w-1/2">
+                                    <label for="training_date" class="block text-sm font-medium text-gray-700">Tanggal
+                                        Pelatihan</label>
+                                    <input type="date" id="training_date" name="training_date"
+                                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-amber-500 focus:border-amber-500 sm:text-sm"
+                                        required>
+                                </div>
+
+                                <!-- lokasi -->
+                                <div class="mb-4 w-1/2">
+                                    <label for="location" class="block text-sm font-medium text-gray-700">Lokasi</label>
+                                    <input type="text" id="location" name="location"
+                                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-amber-500 focus:border-amber-500 sm:text-sm"
+                                        required>
+                                </div>
+
+                                <!-- Jenis Pelatihan -->
+                                <div class="mb-4 w-1/2">
+                                    <label for="type" class="block text-sm font-medium text-gray-700">Jenis
+                                        Pelatihan</label>
+                                    <input type="text" id="type" name="type"
+                                        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-amber-500 focus:border-amber-500 sm:text-sm"
+                                        required>
+                                </div>
+                            </div>
+
+                            <!-- Link pelatihan -->
+                            <div class="mb-4">
+                                <label for="link" class="block text-sm font-medium text-gray-700">Link
+                                    Pelatihan</label>
+                                <input type="url" id="link" name="link"
+                                    class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-amber-500 focus:border-amber-500 sm:text-sm"
+                                    required>
+                            </div>
+                        </div>
+
+                        <!-- Bagian untuk input gambar pelatihan -->
+                        <div class="flex-none self-start pl-10">
+                            <div class="mb-4">
+                                <label for="image" class="block text-sm font-medium text-gray-700">
+                                    Foto Pelatihan
+                                </label>
+                                <div>
+                                    <label for="image"
+                                        class="inline-block cursor-pointer bg-cyan-300 text-white py-16 px-36 rounded-md hover:bg-cyan-400">
+                                        ➕
+                                    </label>
+                                    <input type="file" id="image" name="image" accept="image/*" class="hidden"
+                                        onchange="displayFileName()">
+                                </div>
+                                <div id="file-name" class="text-sm text-gray-500 mt-2"></div>
+                                <!-- Tempat untuk menampilkan nama file -->
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <!-- Submit Button -->
+                    <div class="mt-6">
+                        <button type="submit"
+                            class="bg-cyan-300 text-white py-2 px-4 rounded-md hover:bg-cyan-400 transition duration-200">
+                            Tambah Berita
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+<script>
+    function displayFileName() {
+        const input = document.getElementById('image');
+        const fileNameDisplay = document.getElementById('file-name');
+        const fileName = input.files[0] ? input.files[0].name : 'Tidak ada file yang dipilih';
+        fileNameDisplay.textContent = fileName;
+    }
+</script>
+
+</html>
