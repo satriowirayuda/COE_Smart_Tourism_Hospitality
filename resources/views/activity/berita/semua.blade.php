ba579fb8190bd52fcf2895633424ec7b7861465e
Mariana <marmar.smsr@gmail.com>
2025-01-07 11:25:17 +0700

    fix berita

--- resources/views/activity/berita/semua.blade.php
+++ resources/views/activity/berita/semua.blade.php
@@ -15,74 +15,74 @@
         <a href="{{url('berita/penghargaan')}}">Penghargaan</a>
     </div>
 
+    <!-- <section class="mx-5 md:mx-5 lg:mx-[110px] mb-28 space-y-5 font-lota1">
+        <div class="flex gap-2 md:gap-4 lg:gap-4">
+            <div class="w-[460px] h-[120px] md:w-[560px] md:h-[160px] lg:w-[560px] lg:h-[200px] rounded-xl lg:rounded-2xl shadow-2xl overflow-hidden">
+                <img src="{{ asset('assets/artikel-2.png')}}" alt="artikel" 
+                    class="w-full h-full object-cover flex-shrink-0">
+            </div>
+    
+            <div class="bg-white shadow-2xl rounded-xl lg:rounded-2xl px-2 md:px-7 lg:px-7 py-4"> 
+
+                <div class="flex gap-2">
+                    <div class="flex gap-1 items-center justify-center border border-[#D9D9D9] w-24 md:w-[130px] lg:w-36 h-6 md:h-8 lg:h-8 rounded-md">
+                        <img class="w-4 md:w-6 lg:w-8" src="{{ asset('assets/calender-grey.svg') }}" alt="">
+                        <p class="text-[8px] md:text-[10px] lg:text-xs font-light">20 Agustus-2024</p>
+                    </div>
+                        
+                    <div class=" text-center content-center text-[8px] md:text-[10px] lg:text-xs font-light border border-[#D9D9D9] w-12 md:w-16 lg:w-20 h-6 md:h-8 lg:h-8 rounded-md">
+                        <ul class="list-disc list-inside p-0 m-0">
+                            <li>Artikel</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="space-y-1 mt-2 md:mt-3 lg:mt-3">
+                    <p class="text-[8px] md:text-[10px] lg:text-xs font-bold line-clamp-1">Kemenparekraf Tingkatkan Kunjungan Wisatawan ke Keraton Kasepuhan Cirebon</p>
+                    <p class="text-[8px] md:text-[10px] lg:text-xs font-light line-clamp-2">CIREBON (BeritaTrans.com) - Kementerian Pariwisata dan Ekonomi Kreatif/Badan Pariwisata dan Ekonomi Kreatif (Kemenparekraf/Baparekraf) bersama Center of Excellence Smart Tourism and Hospitality, PT Grhayasa Nusacitra Estima,CIREBON (BeritaTrans.com) - Kementerian Pariwisata dan Ekonomi Kreatif/Badan Pariwisata dan Ekonomi Kreatif (Kemenparekraf/Baparekraf) bersama Center of Excellence Smart Tourism and Hospitality, PT Grhayasa Nusacitra Estima,</p>
+
+                    <div class="flex gap-2 object-center text-[10px] md:text-sm lg:text-base text-black">
+                        <a href="https://www.beritatrans.com/artikel/249965/Kemenparekraf-Tingkatkan-Kunjungan-Wisatawan-ke-Keraton-Kasepuhan-Cirebon/" target="_blank">Selengkapnya</a>
+                        <img src="{{ asset('assets/arrow-b.svg') }}" class="h-4 md:h-6 lg:h-7">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section> -->
+
     <section class="mx-5 md:mx-5 lg:mx-[110px] mb-28 space-y-5 font-lota1">
-        <div class="flex gap-2 md:gap-4 lg:gap-4">
-            <div class="w-[460px] h-[120px] md:w-[560px] md:h-[160px] lg:w-[560px] lg:h-[200px] rounded-xl lg:rounded-2xl shadow-2xl overflow-hidden">
+    <div class="flex gap-2 md:gap-4 lg:gap-4">
+    <div class="w-full max-w-[200px] md:max-w-[300px] lg:max-w-[400px] h-[120px] md:h-[160px] lg:h-[200px] rounded-xl lg:rounded-2xl shadow-2xl overflow-hidden flex-shrink-0">
             <img src="{{ asset('assets/artikel-2.png')}}" alt="artikel" 
                 class="w-full h-full object-cover">
+        </div>
+        <div class="bg-white shadow-2xl rounded-xl lg:rounded-2xl px-2 md:px-7 lg:px-7 py-4 flex-grow">
+            <div class="flex gap-2">
+                <div class="flex gap-1 items-center justify-center border border-[#D9D9D9] w-24 md:w-[130px] lg:w-36 h-6 md:h-8 lg:h-8 rounded-md">
+                    <img class="w-4 md:w-6 lg:w-8" src="{{ asset('assets/calender-grey.svg') }}" alt="">
+                    <p class="text-[8px] md:text-[10px] lg:text-xs font-light">20 Agustus-2024</p>
+                </div>
+
+                <div class="text-center content-center text-[8px] md:text-[10px] lg:text-xs font-light border border-[#D9D9D9] w-12 md:w-16 lg:w-20 h-6 md:h-8 lg:h-8 rounded-md">
+                    <ul class="list-disc list-inside p-0 m-0">
+                        <li>Artikel</li>
+                    </ul>
+                </div>
             </div>
-  
-            <div class="bg-white shadow-2xl rounded-xl lg:rounded-2xl px-2 md:px-7 lg:px-7 py-4"> 
 
-                <div class="flex gap-2">
-                    <div class="flex gap-1 items-center justify-center border border-[#D9D9D9] w-24 md:w-[130px] lg:w-36 h-6 md:h-8 lg:h-8 rounded-md">
-                        <img class="w-4 md:w-6 lg:w-8" src="{{ asset('assets/calender-grey.svg') }}" alt="">
-                        <p class="text-[8px] md:text-[10px] lg:text-xs font-light">20 Agustus-2024</p>
-                    </div>
-                        
-                    <div class=" text-center content-center text-[8px] md:text-[10px] lg:text-xs font-light border border-[#D9D9D9] w-12 md:w-16 lg:w-20 h-6 md:h-8 lg:h-8 rounded-md">
-                        <ul class="list-disc list-inside p-0 m-0">
-                            <li>Artikel</li>
-                        </ul>
-                    </div>
+            <div class="space-y-1 mt-2 md:mt-3 lg:mt-3">
+                <p class="text-[8px] md:text-[10px] lg:text-xs font-bold line-clamp-1">Kemenparekraf Tingkatkan Kunjungan Wisatawan ke Keraton Kasepuhan Cirebon</p>
+                <p class="text-[8px] md:text-[10px] lg:text-xs font-light line-clamp-2">CIREBON (BeritaTrans.com) - Kementerian Pariwisata dan Ekonomi Kreatif/Badan Pariwisata dan Ekonomi Kreatif (Kemenparekraf/Baparekraf) bersama Center of Excellence Smart Tourism and Hospitality, PT Grhayasa Nusacitra Estima,CIREBON (BeritaTrans.com) - Kementerian Pariwisata dan Ekonomi Kreatif/Badan Pariwisata dan Ekonomi Kreatif (Kemenparekraf/Baparekraf) bersama Center of Excellence Smart Tourism and Hospitality, PT Grhayasa Nusacitra Estima,</p>
+
+                <div class="pt-4 flex gap-2 object-center text-[10px] md:text-sm lg:text-base text-black">
+                    <a href="https://www.beritatrans.com/artikel/249965/Kemenparekraf-Tingkatkan-Kunjungan-Wisatawan-ke-Keraton-Kasepuhan-Cirebon/" target="_blank">Selengkapnya</a>
+                    <img src="{{ asset('assets/arrow-b.svg') }}" class="h-4 md:h-6 lg:h-7">
                 </div>
-
-                <div class="space-y-1 mt-2 md:mt-3 lg:mt-3">
-                    <p class="text-[8px] md:text-[10px] lg:text-xs font-bold line-clamp-1">Telkom University Luncurkan Teknologi LOTUS, Ciptakan Video Hasil AI Mengangkat Sejarah Kasepuhan Cirebon</p>
-                    <p class="text-[8px] md:text-[10px] lg:text-xs font-light line-clamp-2">Kehadiran teknologi yang makin canggih seperti kecerdasan buatan (Artificial Intelligence/AI) dapat membantu manusia menciptakan produk yang unik salah satunya inovasi membuat video hasil AI.</p>
-
-                    <div class="flex gap-2 object-center text-[10px] md:text-sm lg:text-base text-black">
-                        <a href="#" target="_blank">Selengkapnya</a>
-                        <img src="{{ asset('assets/arrow-b.svg') }}" class="h-4 md:h-6 lg:h-7">
-                    </div>
-                </div>
-
             </div>
         </div>
+    </div>
+</section>
 
-        <div class="flex gap-2 md:gap-4 lg:gap-4">
-            <div class="w-[460px] h-[120px] md:w-[560px] md:h-[160px] lg:w-[560px] lg:h-[200px] rounded-xl lg:rounded-2xl shadow-2xl overflow-hidden">
-            <img src="{{ asset('assets/artikel-2.png')}}" alt="artikel" 
-                class="w-full h-full object-cover">
-            </div>
-  
-            <div class="bg-white shadow-2xl rounded-xl lg:rounded-2xl px-2 md:px-7 lg:px-7 py-4"> 
-
-                <div class="flex gap-2">
-                    <div class="flex gap-1 items-center justify-center border border-[#D9D9D9] w-24 md:w-[130px] lg:w-36 h-6 md:h-8 lg:h-8 rounded-md">
-                        <img class="w-4 md:w-6 lg:w-8" src="{{ asset('assets/calender-grey.svg') }}" alt="">
-                        <p class="text-[8px] md:text-[10px] lg:text-xs font-light">20 Agustus-2024</p>
-                    </div>
-                        
-                    <div class=" text-center content-center text-[8px] md:text-[10px] lg:text-xs font-light border border-[#D9D9D9] w-12 md:w-16 lg:w-20 h-6 md:h-8 lg:h-8 rounded-md">
-                        <ul class="list-disc list-inside p-0 m-0">
-                            <li>Artikel</li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="space-y-1 mt-2 md:mt-3 lg:mt-3">
-                    <p class="text-[8px] md:text-[10px] lg:text-xs font-bold line-clamp-1">Telkom University Luncurkan Teknologi LOTUS, Ciptakan Video Hasil AI Mengangkat Sejarah Kasepuhan Cirebon</p>
-                    <p class="text-[8px] md:text-[10px] lg:text-xs font-light line-clamp-2">Kehadiran teknologi yang makin canggih seperti kecerdasan buatan (Artificial Intelligence/AI) dapat membantu manusia menciptakan produk yang unik salah satunya inovasi membuat video hasil AI.</p>
-
-                    <div class="flex gap-2 object-center text-[10px] md:text-sm lg:text-base text-black">
-                        <a href="#" target="_blank">Selengkapnya</a>
-                        <img src="{{ asset('assets/arrow-b.svg') }}" class="h-4 md:h-6 lg:h-7">
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </section>
 </body>
 </html>
