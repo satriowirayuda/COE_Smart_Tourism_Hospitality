b8e0aa090fbc35329c1e6148e524d96714c3ea49
Mariana <marmar.smsr@gmail.com>
2025-01-09 22:16:07 +0700

    Membuat Responsif Bisnis

--- resources/views/activity/bisnis/banner.blade.php
+++ resources/views/activity/bisnis/banner.blade.php
@@ -13,15 +13,15 @@
     <x-white-navigation />
         <div class="absolute w-full bg-biru-sth h-[230px] lg:h-[340px] top-0 left-0 -z-10"></div>
 
-        <div class="relative w-full h-[181px] md:[300px] lg:h-[350px] px-4 lg:px-28 mx-auto">
+        <div class="relative w-full h-[170px] md:h-[270px] lg:h-[350px] px-5 md:px-10 lg:px-28">
 
-            <img class="w-full h-full object-cover rounded-[40px] sm:rounded-[30px] lg:rounded-[40px]"
+            <img class="w-full h-full object-cover rounded-[35px] sm:rounded-[40px] lg:rounded-[40px]"
                 src="{{ asset('assets/bisnisbanner.png') }}" alt="">
 
             <div class="absolute inset-0 flex flex-col justify-center items-center text-white">
                 <div class="text-center font-extralight">
-                    <p class="text-base sm:text-xl md:text-4xl lg:text-6xl font-bold">Bisnis</p>
-                    <p class="text-3xl sm:text-3xl md:text-3xl lg:text-3xl italic">Center of Excellence Smart Tourism & Hospitality</p>
+                    <p class="text-lg sm:text-xl md:text-4xl lg:text-6xl font-bold">Bisnis</p>
+                    <p class="text-base sm:text-3xl md:text-3xl lg:text-3xl italic">Center of Excellence Smart Tourism & Hospitality</p>
                 </div>
             </div>
 
